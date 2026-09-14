3ba8866c7cb9890d2bc01a61e9efe08018f5c17f
Manthan997 <manthanparsekar997@gmail.com>
2026-09-14 23:17:09 +0530

    update: fire up the code editors; task 1 is live!!!

---
+++ Task_1/Task_1A/t1a_fs_pwm/code/frequency_scaling.v
@@ -0,0 +1,36 @@
+// Logic Quest Bot : Task 1A : Frequency Scaling
+/*
+Instructions
+-------------------
+Students are not allowed to make any changes in the Module declaration.
+This file is used to design a module which will scale down the 50MHz Clock Frequency to clk_5MHz
+
+Recommended Quartus Version : 20.1
+The submitted project file must be 20.1 compatible as the evaluation will be done on Quartus Prime Lite 20.1.
+
+Warning: The error due to compatibility will not be entertained.
+-------------------
+*/
+
+//Frequency Scaling
+//Inputs : clk_50MHz
+//Output : 5MHz
+
+
+module frequency_scaling (
+    input clk_50MHz,
+    input reset_n,
+    output reg clk_5MHz
+);
+
+//////////////////DO NOT MAKE ANY CHANGES ABOVE THIS LINE //////////////////
+
+
+/*
+ add your code here 
+ */
+
+//////////////////DO NOT MAKE ANY CHANGES BELOW THIS LINE //////////////////
+
+endmodule
+
---
+++ Task_1/Task_1A/t1a_fs_pwm/code/pwm_generator.v
@@ -0,0 +1,35 @@
+// Logic Quest Bot : Task 1A : PWM Generator
+/*
+Instructions
+-------------------
+Students are not allowed to make any changes in the Module declaration.
+This file is used to design a module which will scale down the clk_5MHz Clock Frequency to 500Hz and perform Pulse Width Modulation on it.
+
+Recommended Quartus Version : 20.1
+The submitted project file must be 20.1 compatible as the evaluation will be done on Quartus Prime Lite 20.1.
+
+Warning: The error due to compatibility will not be entertained.
+-------------------
+*/
+
+//PWM Generator
+//Inputs : clk_5MHz, pulse_width
+//Output : clk_500Hz, pwm_signal
+
+module pwm_generator(
+    input clk_5MHz,
+    input reset_n,
+    input [4:0] pulse_width,
+    output reg clk_500Hz, pwm_signal
+);
+
+//////////////////DO NOT MAKE ANY CHANGES ABOVE THIS LINE //////////////////
+
+/*
+ add your code here 
+ */
+ 
+//////////////////DO NOT MAKE ANY CHANGES BELOW THIS LINE//////////////////
+
+endmodule
+
